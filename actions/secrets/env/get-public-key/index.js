a22ebf431a3492e3a785d3efef95f8b88d30beba
Tobias Weinlich (Tobiriz) <tobias.weinlich@gmx.de>
2023-11-27 13:48:16 +0100

    add logs

--- actions/secrets/env/get-public-key/index.js
+++ actions/secrets/env/get-public-key/index.js
@@ -3,18 +3,24 @@ import github from '@actions/github';
 import { Octokit } from 'octokit';
 
 async function run() {
+    const api = core.getInput('githubApi');
+    const token = core.getInput('token');
+    const repo = core.getInput('repositoryId');
+    const env = core.getInput('environmentName');
+    console.log("Using URL: " + api + "/repositories/" + repo + "/environments/" + env + "/secrets/public-key");
     try {
         const octokit = new Octokit({
-            baseUrl: core.getInput('githubApi'),
-            auth: core.getInput('token')
+            baseUrl: api,
+            auth: token
           });
         const response = await octokit.request('GET /repositories/{repository_id}/environments/{environment_name}/secrets/public-key', {
-            repository_id: core.getInput('repositoryId'),
-            environment_name: core.getInput('environmentName'),
+            repository_id: repo,
+            environment_name: env,
             headers: {
                 'X-GitHub-Api-Version': '2022-11-28'
             }
         });
+        console.log("Response: " + JSON.stringify(response));
         core.setOutput("key", response.key);
         core.setOutput("keyId", response.key_id);
     } catch (error) {
